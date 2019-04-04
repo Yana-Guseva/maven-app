e1a7f33a299a669ad7de6679434ab96e2cfcfb00
Yana <9301YanaGuseva@gmail.com>
2019-04-04 03:12:01 +0300

    Hibernate started

---
+++ module1/src/main/java/dogapp/dao/HibernateDogDao.java
@@ -0,0 +1,49 @@
+package dogapp.dao;
+
+import dogapp.dto.Dog;
+import dogapp.exception.DogNotFoundException;
+import lombok.AllArgsConstructor;
+import org.hibernate.SessionFactory;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+public class HibernateDogDao implements DogDao {
+
+    private final SessionFactory sessionFactory;
+
+    @Override
+    public Dog get(UUID id) {
+        Dog dog = sessionFactory.getCurrentSession().get(Dog.class, id);
+        if (dog == null) {
+            throw new DogNotFoundException(id);
+        }
+        return dog;
+    }
+
+    @Override
+    public Dog create(Dog dog) {
+        dog.setId(UUID.randomUUID());
+        sessionFactory.getCurrentSession().save(dog);
+        return dog;
+    }
+
+    @Override
+    public Dog update(Dog dog) {
+        Dog oldDog = sessionFactory.getCurrentSession().get(Dog.class, dog.getId());
+        if (oldDog == null) {
+            throw new DogNotFoundException(dog.getId());
+        }
+        sessionFactory.getCurrentSession().update(dog);
+        return dog;
+    }
+
+    @Override
+    public void delete(UUID id) {
+        Dog dog = sessionFactory.getCurrentSession().get(Dog.class, id);
+        if (dog == null) {
+            throw new DogNotFoundException(id);
+        }
+        sessionFactory.getCurrentSession().delete(dog);
+    }
+}
